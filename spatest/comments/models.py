c5976705ea5093e1ca89871eae889042a4e25a3c
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2025-03-11 18:43:48 +0200

    created db models and add get hash sum for files

--- spatest/comments/models.py
+++ spatest/comments/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+User.add_to_class("email", models.EmailField(null=False, blank=False))
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    class Meta:
+        db_table = "profiles"
+        verbose_name = "User profile"
+        verbose_name_plural = "Users profiles"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user")
+    home_page = models.URLField(null=True, blank=True, verbose_name="home page")
+
+
+class FileUpload(models.Model):
+    class Meta:
+        db_table = "files_uploads"
+        verbose_name = "File upload"
+        verbose_name_plural = "Files uploads"
+
+    filename = models.FileField(upload_to="static/")
+    file_type = models.CharField(max_length=4)
+    hash_sum = models.CharField(max_length=64, unique=True)
+
+
+class Comment(models.Model):
+    class Meta:
+        db_table = "comments"
+        verbose_name = "User comment"
+        verbose_name_plural = "Users comments"
+
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, verbose_name="user")
+    text = models.TextField(null=False, blank=False, verbose_name="text comment")
+    files = models.ManyToManyField(
+        FileUpload, db_table="comments_files_associations",
+        null=True, blank=True, verbose_name="files in comment"
+    )
+    date_time = models.DateTimeField(auto_now_add=True, verbose_name="date and time")
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- spatest/services/base.py
+++ spatest/services/base.py
@@ -1,4 +1,6 @@
 import os
+import hashlib
+from typing import Literal, Optional
 
 from config import Config
 
@@ -9,6 +11,17 @@ class BaseManager:
         self.config: Config = config
         self.filename: str = filename
 
+        self.name_and_ext = os.path.splitext(self.filename)
+
     def check_extension(self, allowed_types: list[str]) -> bool:
-        name_and_ext = os.path.splitext(self.filename)
-        return name_and_ext[1].upper() in allowed_types if len(name_and_ext) > 1 else False
+        return self.name_and_ext[1].upper() in allowed_types if len(self.name_and_ext) > 1 else False
+
+    def get_hash_sum(self, file_mode: Literal["r", "rb"]) -> str:
+        file_hash = hashlib.sha256()
+        file_encoding: Optional[str] = "utf-8" if file_mode == "r" else None
+
+        with open(self.filename, mode=file_mode, encoding=file_encoding) as data_file:
+            content = data_file.read()
+            file_hash.update(content)
+
+        return file_hash.hexdigest()
